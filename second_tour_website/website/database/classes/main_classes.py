b8052da752d6fd334f38b655d05a7f4f93ec5222
Max Descroix <max.descroix@etudiant.univ-lr.fr>
2021-10-27 13:02:56 +0200

    Added TABLE SERIE

--- second_tour_website/website/database/classes/main_classes.py
+++ second_tour_website/website/database/classes/main_classes.py
@@ -1,3 +1,4 @@
+from ctypes import resize
 from ..main_database import db
 
 class UTILISATEURS(db.Model):
@@ -24,4 +25,27 @@ class UTILISATEURS(db.Model):
         user = UTILISATEURS.query.filter_by(email=email, admin=admin).first()
         if user:
             return ["Cet utilisateur existe déjà", "danger"]
+        return False
+
+class SERIE(db.Model):
+    __tablename__=  'SERIE'
+    id_serie = db.Column('id', db.Integer, primary_key = True)
+    nom = db.Column(db.String(40), nullable=False)
+    specialite1 = db.Column(db.String(50),nullable=False)
+    specialite2 = db.Column(db.String(50),nullable=False)
+
+    def __init__(self, nom, specialite1, specialite2):
+        self.unvalid = False
+
+        if res := self.unique_nom_spe1_spe2(nom, specialite1, specialite2):
+            self.unvalid = res
+
+        self.nom = nom
+        self.specialite1 = specialite1
+        self.specialite2 = specialite2
+
+    def unique_nom_spe1_spe2(self, nom, spe1, spe2):
+        serie = SERIE.query.filter_by(nom=nom, specialite1=spe1, specialite2=spe2).first()
+        if serie:
+            return ["Cette serie existe déja", "danger"]
         return False
