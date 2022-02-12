346ad2f2a8f57e4b6c8d3b3ad72372d377ddeb98
Louis Huort <louis.huort@etudiant.univ-lr.fr>
2022-02-12 18:32:17 +0100

    Separate api and website in 2 container & fix footer bugs due to vue js

--- stack/build/website/start.sh
+++ stack/build/website/start.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 service nginx start
-cd /app/api/secondtour_api && uvicorn main:app --host 0.0.0.0 --port 443 &
 cd /app/website/secondtour_website && uwsgi /app/website/secondtour_website/website.ini
