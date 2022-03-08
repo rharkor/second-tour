ded2266b2cb5d982c6cdd204c8bbf2298c70fb05
Louis Huort <louis.huort@etudiant.univ-lr.fr>
2022-03-08 17:01:21 +0100

    Fix bugs api api

--- stack/api/secondtour_api_v2/routes/basics_routes.js
+++ stack/api/secondtour_api_v2/routes/basics_routes.js
@@ -1,33 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-/**
- * @swagger
- * /api/fetchou/{table}:
- *    post:
- *      tags:
- *          - Automated Data Operation
- *      description: Return all the rows of a table
- *      parameters:
- *          - in: path
- *            name: table
- *            type: string
- *            required: true
- *            description: The table you want to fetch
- *          - in: body
- *            name: User auth
- *            type: object
- *            schema:
- *              $ref: '#definitions/User'
- *      responses:
- *          '200':
- *              description: Table retreive correctly
- *              schema:
- *                  $ref: '#definitions/fetchOut'
- *          '401':
- *              description: Your authentification identifiers are not correct
- */
- router.route('/fetchou/:table').post(async (req, res) => {
+ router.route('/wow').post(async (req, res) => {
   //  console.log("here")
   // let table = req.params['table']
   // let result = await db.query(`SELECT * FROM ${table};`).catch(e => {
